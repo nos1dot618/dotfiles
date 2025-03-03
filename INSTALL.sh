b9ad9c9756b4eab7754473d0b86afda884d830b3
ninthcircle <mainlakshayhoon@gmail.com>
2025-03-03 17:57:18 +0530

    add 'playerctl' to i3wm config

--- INSTALL.sh
+++ INSTALL.sh
@@ -4,7 +4,7 @@ TOOLS=(
 	tmux
 	xclip # for copying to clipboard in tmux
 	emacs
-	nitrogen brightnessctl gnome-screenshot # for i3wm
+	nitrogen brightnessctl gnome-screenshot playerctl # for i3wm
 )
 
 sudo apt install $TOOLS[@]
