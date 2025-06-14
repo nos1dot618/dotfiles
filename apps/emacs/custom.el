4b7ca48baaf78665761f5c46cd9714024424ba52
ninthcircle <mainlakshayhoon@gmail.com>
2025-06-14 20:29:11 +0530

    [emacs] update emacs config

--- apps/emacs/custom.el
+++ apps/emacs/custom.el
@@ -3,11 +3,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(modus-vivendi-tritanopia))
+ '(custom-enabled-themes '(gruber-darker))
  '(custom-safe-themes
-   '("09b833239444ac3230f591e35e3c28a4d78f1556b107bafe0eb32b5977204d93"
+   '("01a9797244146bbae39b18ef37e6f2ca5bebded90d9fe3a2f342a9e863aaa4fd"
+     "09b833239444ac3230f591e35e3c28a4d78f1556b107bafe0eb32b5977204d93"
      default))
- '(package-selected-packages nil))
+ '(package-selected-packages
+   '(company gruber-darker-theme markdown-mode multiple-cursors nix-mode
+             smex zenburn-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
