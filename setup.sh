c1adb012ac21a604ecd8c93a0b8eaebf9b1b9fe5
ninthcircle <mainlakshayhoon@gmail.com>
2025-06-15 12:11:21 +0530

    [setup] source vars.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 set -xeu
-
-export DOTFILES_ROOT=$(pwd)
-export PROFILE="$DOTFILES_ROOT/profiles/orava-popos"
-# NOTE: This is need such that superuser scripts does not assume the user 'root'
-#       and home '/root', when running commands as superuser.
-export MY_USER="$USER"
-export MY_HOME="$HOME"
+source vars.sh
 
 bash "$PROFILE/setup.sh" || true
