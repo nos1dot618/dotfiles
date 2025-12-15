cb04a2cd6e730d52d13232d6947f986663cdb4a2
ninthcircle <mainlakshayhoon@gmail.com>
2025-12-16 00:34:44 +0530

    Update Microsoft.PowerShell_profile.ps1 for nosferatu-win profile

--- profiles/nosferatu-win/Microsoft.PowerShell_profile.ps1
+++ profiles/nosferatu-win/Microsoft.PowerShell_profile.ps1
@@ -6,6 +6,8 @@ $AddPaths = @(
     "$env:USERPROFILE\ThirdParty\ideaIC-2025.1.3.win\bin"
     "$env:USERPROFILE\ThirdParty\DB.Browser.for.SQLite-v3.13.1-win64"
     "$env:USERPROFILE\ThirdParty\rclone-v1.71.2-windows-amd64"
+    "$env:USERPROFILE\ThirdParty\typst"
+    "$env:USERPROFILE\ThirdParty\microsoft-terminal"
 )
 
 $CurrentUserPath = [Environment]::GetEnvironmentVariable("PATH", "User")
@@ -27,3 +29,4 @@ foreach ($Path in $AddPaths) {
 
 # Aliases
 Set-Alias ghc "ghc-9.8.2.exe"
+Set-Alias gti git # For typos ;)
