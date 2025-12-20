3dabeef404535ebb6a294e4b12c08e3c78d7b485
ninthcircle <mainlakshayhoon@gmail.com>
2025-12-20 20:03:06 +0530

    Update powershell config

--- profiles/nosferatu-win/Microsoft.PowerShell_profile.ps1
+++ profiles/nosferatu-win/Microsoft.PowerShell_profile.ps1
@@ -1,31 +1,31 @@
 $AddPaths = @(
-    "$env:USERPROFILE\ThirdParty\emacs-30.1\bin"
-    "$env:USERPROFILE\ThirdParty\focus"
-    "$env:USERPROFILE\ThirdParty\openjdk-24.0.1_windows-x64_bin\jdk-24.0.1\bin"
-    "$env:USERPROFILE\ThirdParty\nasm-2.16.03"
-    "$env:USERPROFILE\ThirdParty\ideaIC-2025.1.3.win\bin"
-    "$env:USERPROFILE\ThirdParty\DB.Browser.for.SQLite-v3.13.1-win64"
-    "$env:USERPROFILE\ThirdParty\rclone-v1.71.2-windows-amd64"
-    "$env:USERPROFILE\ThirdParty\typst"
-    "$env:USERPROFILE\ThirdParty\microsoft-terminal"
+    "$env:USERPROFILE\Thirdparty\emacs-30.1\bin\"
+    "$env:USERPROFILE\Thirdparty\focus\"
+    "$env:USERPROFILE\Thirdparty\openjdk-24.0.1\bin\"
+    "$env:USERPROFILE\Thirdparty\nasm-2.16.03\"
+    "$env:USERPROFILE\Thirdparty\clion-2025.1.3\bin\"
+    "$env:USERPROFILE\Thirdparty\db-browser-for-sqlite-3.13.1\"
+    "$env:USERPROFILE\Thirdparty\rclone-1.71.2\"
+    "$env:USERPROFILE\Thirdparty\typst\"
+    "$env:USERPROFILE\Thirdparty\microsoft-terminal\"
 )
 
 $CurrentUserPath = [Environment]::GetEnvironmentVariable("PATH", "User")
 
 foreach ($Path in $AddPaths) {
     if ($env:PATH -notlike "*$Path*") {
-        Write-Host "INFO: Adding '$Path' to Session PATH"
+        Write-Host "INFO: Adding '$Path' to Session PATH."
         $env:PATH += ";$Path"
     }
     if ($CurrentUserPath -notlike "*$Path*") {
-        Write-Host "INFO: Adding '$Path' to Persistent User PATH"
+        Write-Host "INFO: Adding '$Path' to Persistent User PATH."
         $CurrentUserPath += ";$Path"
     }
 }
 
 # Set environment variables
 [Environment]::SetEnvironmentVariable("PATH", $CurrentUserPath, "User")
-[Environment]::SetEnvironmentVariable("JAVA_HOME", "$env:USERPROFILE\ThirdParty\openjdk-24.0.1_windows-x64_bin\jdk-24.0.1", "User")
+[Environment]::SetEnvironmentVariable("JAVA_HOME", "$env:USERPROFILE\Thirdparty\openjdk-24.0.1\", "User")
 
 # Aliases
 Set-Alias ghc "ghc-9.8.2.exe"
