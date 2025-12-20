1b5a5f0b08a10cf80dc1b47415a1ee51f1e02a4d
ninthcircle <mainlakshayhoon@gmail.com>
2025-12-20 13:43:12 +0530

    Comment emacs setup

--- profiles/orava/setup.sh
+++ profiles/orava/setup.sh
@@ -8,7 +8,7 @@ sudo -E bash "$DOTFILES_ROOT/profiles/orava/install.sh"
 bash "$DOTFILES_ROOT/desktop/setup.sh"
 
 bash "$DOTFILES_ROOT/apps/bash/setup.sh"
-bash "$DOTFILES_ROOT/apps/emacs/setup.sh"
+# bash "$DOTFILES_ROOT/apps/emacs/setup.sh"
 bash "$DOTFILES_ROOT/apps/fish/setup.sh"
 bash "$DOTFILES_ROOT/apps/git/setup.sh"
 bash "$DOTFILES_ROOT/apps/ssh/setup.sh"
