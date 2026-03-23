05d02f254cf236ac6e17aa82684bb8fa9a6a8a49
ninthcircle <mainlakshayhoon@gmail.com>
2026-03-24 01:56:33 +0530

    Add Orava-KDE specific applications.

---
+++ profiles/orava-kde/applications/tesseract-ocr/setup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -eu
+source "$DOTFILES_ROOT/apps/bash/commons.sh"
+
+install_package tesseract-ocr tesseract-ocr-eng wl-clipboard
+
+chmod +x "$DOTFILES_ROOT/profiles/orava-kde/applications/tesseract-ocr/ocr.sh"
+
+log_info "Set up OCR using \"tesseract\" successfully."
--- profiles/orava-kde/setup.sh
+++ profiles/orava-kde/setup.sh
@@ -4,8 +4,12 @@ source "$DOTFILES_ROOT/apps/bash/commons.sh"
 
 bash "$DOTFILES_ROOT/profiles/orava/setup.sh"
 
-sudo -E bash "$DOTFILES_ROOT/profiles/orava-kde/install.sh"
+sudo -E bash "$PROFILE/install.sh"
 
 # bash "$DOTFILES_ROOT/profiles/orava-kde/autostart.sh"
+
+bash "$PROFILE/applications/spectacle/setup.sh"
+bash "$PROFILE/applications/tesseract-ocr/setup.sh"
+log_info "Set up \"Orave-KDE\" specific applications successfully."
 
 log_info "Set up profile \"Orave-KDE\" successfully."
