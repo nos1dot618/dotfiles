dd0f1dd7ef1f314e9c293532e46b1d7417e30a1e
ninthcircle <mainlakshayhoon@gmail.com>
2026-06-11 01:18:55 +0530

    New PowerShell function for pruning a Git repository.

--- applications/git/Utils.ps1
+++ applications/git/Utils.ps1
@@ -1,6 +1,7 @@
 . (Join-Path $env:DOTFILES_ROOT "commons\Utils.ps1")
 
-function Git-Push {
+function Invoke-GitPush {
+    [CmdletBinding()]
     param(
         [Parameter(ValueFromRemainingArguments = $true)]
         [string[]]$Args
@@ -12,4 +13,21 @@ function Git-Push {
         if ($Args.Count -eq 0) { git push $Remote HEAD }
         else { git push $Remote @Args }
     }
+}
+
+function Invoke-GitPrune {
+    Info -Message "Removing stale remote-tracking branches..."
+    git fetch --prune
+    Info -Message "Expiring reflogs..."
+    git reflog expire --expire=now --all
+    Info -Message "Pruning unreachable objects..."
+    git gc --prune=now --aggressive
+}
+
+@{
+    "gitp"     = "Invoke-GitPush"
+    "gitpush"  = "Invoke-GitPush"
+    "gitprune" = "Invoke-GitPrune"
+}.GetEnumerator() | ForEach-Object {
+    Set-Alias -Name $_.Key -Value $_.Value
 }
