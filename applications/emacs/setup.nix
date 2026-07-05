7a37fddbaa66cadf051655a860811d3e9fe52a6c
ninthcircle <mainlakshayhoon@gmail.com>
2026-07-05 19:31:33 +0530

    EMACS: New nix configuration.

---
+++ applications/emacs/setup.nix
@@ -0,0 +1,11 @@
+{ config, pkgs, ... }: 
+{
+  programs.emacs = {
+    enable = true;
+  };
+  xdg.configFile."emacs/init.el".source = ./init.el;
+  xdg.configFile."emacs/custom.el".source = ./custom.el;
+  xdg.configFile."emacs/language.el".source = ./language.el;
+  # NOTE: Emacs will only try to sense `~/.config/emacs/`,
+  #       when there is no `~/.emacs` and `~/.emacs.d/`.
+}
